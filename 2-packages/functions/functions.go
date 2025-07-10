1ab91be05e0e59b67407ed437f29f6963563d3c3
BeknazarovTemur <imtemurb@gmail.com>
2025-07-10 18:07:55 +0500

    first commit

---
+++ 2-packages/functions/functions.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+//func add(x, y int) int {
+//	return x + y
+//}
+
+func swap(x, y string) (string, string) {
+	return x, y
+}
+
+func main() {
+	//fmt.Println(add(42, 13))
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
+}
